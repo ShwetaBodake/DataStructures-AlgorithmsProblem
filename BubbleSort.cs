8aef7a38dcb151f49fa49d3e28b6a6a1133a1f71
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-30 12:00:08 +0530

    Sorted Array using Bubble sort

---
+++ BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DataStructurePrograms
+{
+	public class BubbleSort
+	{
+		public void DisplayBubbleSort()
+		{
+			int[] arr = { 11, 22, 334, 54, 76 };
+			int temp;
+
+			for (int j = 0; j <= arr.Length - 2; j++)
+			{
+				for (int i = 0; i <= arr.Length - 2; i++)
+					if (arr[i] > arr[i + 1])
+					{
+						temp = arr[i + 1];
+						arr[i + 1] = arr[i];
+						arr[i] = temp;
+					}
+			}
+
+
+			Console.WriteLine("The sorted array: ");
+			foreach (int p in arr)
+			{
+				Console.WriteLine(p);
+			}
+
+		}
+	}
+}
