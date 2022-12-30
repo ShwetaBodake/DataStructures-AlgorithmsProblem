892358619dac6a809fadead35aa9691ffbad469d
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-30 11:07:36 +0530

    Data Structure Algorithm Programs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace DataStructurePrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            InsertionSort sort = new InsertionSort();
+            sort.DisplayInsertionSort();
+
+            BubbleSort sort1 = new BubbleSort();
+            sort1.DisplayBubbleSort();
+
+            Anagram.isAnagram();
+
+
+
+        }
+    }
+}
