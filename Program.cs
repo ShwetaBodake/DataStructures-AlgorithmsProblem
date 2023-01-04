3dc9d10ac759209a63034a98ffea3ddc21de856f
ShwetaBodake <shwetabodake8108@gmail.com>
2023-01-04 15:07:11 +0530

    Solution of Prime Number within range

--- Program.cs
+++ Program.cs
@@ -6,38 +6,53 @@ namespace DataStructurePrograms
     {
         public static void Main(string[] args)
         {
-
-          
+            PrimeNumbersWithinRange.PrimeNumbers();
+        }
+        public class PrimeNumbersWithinRange
+        {
+            public static void PrimeNumbers()
             {
-                string str = "ABC";
-                char[] charArry = str.ToCharArray();
-                Permute(charArry, 0, 2);
-                Console.ReadKey();
-            }
 
-            static void Permute(char[] arry, int i, int n)
-            {
-                int j;
-                if (i == n)
-                    Console.WriteLine(arry);
-                else
-                {
-                    for (j = i; j <= n; j++)
+                int num, i, ctr, stno, enno;
+
+                Console.Write("\n\n");
+                Console.Write("Find the prime numbers within a range of numbers:\n");
+                Console.Write("---------------------------------------------------");
+                Console.Write("\n\n");
+
+                Console.Write("Input starting number of range: ");
+                stno = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input ending number of range : ");
+                enno = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The prime numbers between {0} and {1} are : \n", stno, enno);
+
+                for (num = stno; num <= enno; num++)
+                    if (num == 0)
+                    { Console.WriteLine("Zero is not a prime number"); }
+                    else
                     {
-                        Swap(ref arry[i], ref arry[j]);
-                        Permute(arry, i + 1, n);
-                        Swap(ref arry[i], ref arry[j]); //backtrack
+                        ctr = 0;
+
+                        for (i = 2; i <= num / 2; i++)
+                        {
+                            if (num % i == 0)
+                            {
+                                ctr++;
+                                break;
+                            }
+                        }
+
+                        if (ctr == 0 && num != 1)
+                            Console.Write("{0} ", num);
                     }
-                }
+                Console.Write("\n");
             }
 
-            static void Swap(ref char a, ref char b)
-            {
-                char tmp;
-                tmp = a;
-                a = b;
-                b = tmp;
-            }
+
+
+
+
+
         }
     }
 }
